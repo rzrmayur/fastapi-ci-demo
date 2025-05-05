051152140c63ded1297fdfd466ef1b6a558d14a9
rzrmayur <rzrmayur@gmail.com>
2025-05-05 19:21:35 +0200

    another

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "CI/CD pipeline is working with an update! AWESOME"}
+    return {"message": "CI/CD pipeline is working with an update! AWESOME, recheck, l1"}
 
